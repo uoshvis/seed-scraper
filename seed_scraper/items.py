11430f124ea94af72bfddb7264a7e8a1068dfd04
uoshvis <uoshvis@gmail.com>
2021-06-11 14:54:28 +0300

    add main functionality

--- seed_scraper/items.py
+++ seed_scraper/items.py
@@ -3,10 +3,58 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+from itemloaders.processors import MapCompose, TakeFirst
 import scrapy
 
 
-class SeedScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class EnforcementItem(scrapy.Item):
+
+    legal_case_name = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    legal_case_detail_url = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    defendant_name = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    defendant_type = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    first_doc_date = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    first_resolution_date = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    allegation_type = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    initial_filling_format = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    case_number = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    federal_district_court = scrapy.Field(
+        input_processor=MapCompose(str.strip),
+        output_processor=TakeFirst()
+    )
+    # sic_code = scrapy.Field(
+    #     input_processor=MapCompose(str.strip),
+    #     output_processor=TakeFirst()
+    # )
+    # cusip = scrapy.Field(
+    #     input_processor=MapCompose(str.strip),
+    #     output_processor=TakeFirst()
+    # )
+
--- seed_scraper/spiders/enforcements.py
+++ seed_scraper/spiders/enforcements.py
@@ -1,4 +1,7 @@
+import os
 import scrapy
+from seed_scraper.items import EnforcementItem
+from scrapy.loader import ItemLoader
 
 
 class EnforcementsSpider(scrapy.Spider):
@@ -6,5 +9,71 @@ class EnforcementsSpider(scrapy.Spider):
     allowed_domains = ['research.seed.law.nyu.edu']
     start_urls = ['https://research.seed.law.nyu.edu/Search/Results']
 
-    def parse(self, response):
+    # output csv field order
+
+    custom_settings = {
+        "FEED_EXPORT_FIELDS": ['legal_case_name',
+                               'defendant_name',
+                               'defendant_type',
+                               'first_doc_date',
+                               'first_resolution_date',
+                               'allegation_type',
+                               'initial_filling_format',
+                               'case_number',
+                               'federal_district_court',
+                               ]
+    }
+
+    try:
+        os.remove('output.csv')
+    except OSError:
         pass
+
+    def parse(self, response):
+        rows = response.xpath('//table//tr')
+        count = 0
+        for row in rows:
+            td = row.xpath('td')
+            if td:
+                count += 1
+                legal_case_detail_url = td[0].xpath('a/@href').get()
+                first_resolution_date = td[4].xpath('text()').get()
+                absolute_url = response.urljoin(legal_case_detail_url)
+                yield scrapy.Request(url=absolute_url,
+                                     callback=self.parse_details,
+                                     cb_kwargs=dict(first_resolution_date=first_resolution_date)
+                                     )
+                # limit counter
+                if count > 20:
+                    break
+
+    def parse_details(self, response, first_resolution_date):
+        loader = ItemLoader(item=EnforcementItem(), selector=response)
+        loader.add_value('first_resolution_date', first_resolution_date)
+        loader.add_xpath('legal_case_name',
+                         '//span[contains(.//text(), "Legal Case Name")]/following-sibling::span/text()')
+        loader.add_value('legal_case_detail_url',
+                         response.url)
+        loader.add_xpath('defendant_name',
+                         '//h2[contains(./span/text(), "Defendant Name")]/span/following-sibling::text()')
+        loader.add_xpath('defendant_type',
+                         '//div[contains(.//text(), "Defendant Type")]/span/text()')
+        loader.add_xpath('first_doc_date',
+                         '//span[contains(.//text(), "First Document Date")]/following-sibling::span/text()')
+        loader.add_xpath('allegation_type',
+                         '//span[contains(.//text(), "Allegation Type")]/following-sibling::span/text()')
+        loader.add_xpath('initial_filling_format',
+                         '//span[contains(.//text(), "Initial Filing Format")]/following-sibling::span/text()')
+        loader.add_xpath('case_number',
+                         '//span[contains(.//text(), "Case Number")]/following-sibling::span/text()')
+        loader.add_xpath('federal_district_court',
+                         '//span[contains(.//text(), "Federal District Court")]/following-sibling::span/text()')
+
+        # mostly empty fields
+
+        # loader.add_xpath('sic_code',
+        #                  '//div[contains(.//text(), "SIC Code")]/span/text()')
+        # loader.add_xpath('cusip',
+        #                  '//div[contains(.//text(), "CUSIP")]/span/text()')
+        yield loader.load_item()
+
